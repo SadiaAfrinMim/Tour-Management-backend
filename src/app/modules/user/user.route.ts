0a1d8ea42c04f2f91dc1117eb9f9b4ca763928b2
SadiaAfrinMim <mimsadia367@gmail.com>
2025-07-10 23:49:59 +0600

    validate request and route ar modde validate korte pari and zod applied 2 way create or update user

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,21 +1,13 @@
-import { NextFunction, Request, Response, Router } from "express";
+
 import { userControllers } from "./user.controller";
-import  { AnyZodObject } from "zod"
+
 import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { Router } from "express";
 
 
 
-const validateRequest = (zodSchema:AnyZodObject)=>async(req:Request,res:Response,next:NextFunction)=>{
-  try{
-       req.body =await zodSchema.parseAsync(req.body)
-   
-    next()
 
-  }catch(error){
-    next(error)
-
-  }
-}
 const router = Router()
 
 router.post("/register",
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { IsActive, Role } from "./user.interface";
 
  export const createUserZodSchema = z.object({
           name:z
@@ -18,6 +19,45 @@ import z from "zod";
             phone:z
             .string({invalid_type_error:"phone number nust be string"})
             .regex(/^(\+88)?01[3-9]\d{8}$/,{message:"Phone number must be valid for Bangladesh.Formet :017XXXXXXXXX or+8801XXXXXx"})
+            .optional(),
+         
+            address:z.string({invalid_type_error:"Address must be string"}).max(200,{message:"Address cannot exceed 200 characters."})
+            .optional()
+        
+
+    })
+
+
+    export const updateUserZodSchema = z.object({
+          name:z
+          .string({invalid_type_error:"Name must be string"})
+          .min(2,{message:"Name too short .Minimum 2 character long"})
+          .max(50,{message:"Name too long"})
+          .optional(),
+         
+            password: z
+            .string({invalid_type_error:"password must be string"})
+            .min(8,{message:"password must be 8 character long"})
+            .regex(/^(?=.*[A-Z])/,{message:"Password must contain at least 1 uppercase letter"})
+            .regex(/^(?=.*[@$!%*?&])/,{message:"password must contain at least 1 special character"})
+            .regex(/^(?=.*\d)/,{message:"password must contain at least 1 number"})
+            .optional(),
+            phone:z
+            .string({invalid_type_error:"phone number nust be string"})
+            .regex(/^(\+88)?01[3-9]\d{8}$/,{message:"Phone number must be valid for Bangladesh.Formet :017XXXXXXXXX or+8801XXXXXx"})
+            .optional(),
+
+            role:z
+            .enum(Object.keys(Role)as[string])
+            .optional(),
+            isActive: z
+            .enum(Object.values(IsActive) as [string])
+            .optional(),
+            isDeleted:z
+            .boolean({invalid_type_error:"isdeleted must be true or false"})
+            .optional(),
+            isverified:z
+            .boolean({invalid_type_error:"isVerified must be true or false"})
             .optional(),
          
             address:z.string({invalid_type_error:"Address must be string"}).max(200,{message:"Address cannot exceed 200 characters."})
